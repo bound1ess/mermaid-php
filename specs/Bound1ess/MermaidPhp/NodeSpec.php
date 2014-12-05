cc4c37994badb2f7b2884299e7129c2c6d02ce9f
Ilya <bound1ess.dev@gmail.com>
2014-12-05 19:34:14 +0600

    Add getId() method for Node class and a couple of styling constants.

--- specs/Bound1ess/MermaidPhp/NodeSpec.php
+++ specs/Bound1ess/MermaidPhp/NodeSpec.php
@@ -3,10 +3,20 @@
 use PhpSpec\ObjectBehavior;
 
 class NodeSpec extends ObjectBehavior {
+	
+	function let()
+	{
+		$this->beConstructedWith('node id');
+	}
 
 	function it_is_initializable()
 	{
 		$this->shouldHaveType('Bound1ess\MermaidPhp\Node');
+	}
+
+	function it_returns_the_node_id()
+	{
+		$this->getId()->shouldReturn('node id');
 	}
 
 }
--- src/Bound1ess/MermaidPhp/Node.php
+++ src/Bound1ess/MermaidPhp/Node.php
@@ -2,6 +2,50 @@
 
 class Node {
 
-	# lovely Node class		
+	/**
+	 * @var string
+	 */
+	const SQUARE_EDGES = '[%s]';
+
+	/**
+	 * @var string
+	 */
+	const ROUND_EDGES = '(%s)';
+
+	/**
+	 * @var string
+	 */
+	const CIRCLE = '((%s))';
+
+	/**
+	 * @var string
+	 */
+	const ASYMETRIC_SHAPE = '>%s]';
+		
+	/**
+	 * @var string
+	 */
+	const RHOMBUS = '{%s}';
+
+	/**
+	 * @var string
+	 */
+	protected $id;
+
+	/**
+	 * @param string $id
+	 */
+	public function __construct($id)
+	{
+		$this->id = $id;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
 
 }
