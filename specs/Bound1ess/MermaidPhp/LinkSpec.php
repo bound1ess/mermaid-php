fa5a5b78006610ff47385155be283cec5778f09f
Ilya <bound1ess.dev@gmail.com>
2014-12-05 20:40:35 +0600

    Add Link class and a spec file for it.

---
+++ specs/Bound1ess/MermaidPhp/LinkSpec.php
@@ -0,0 +1,12 @@
+<?php namespace specs\Bound1ess\MermaidPhp;
+
+use PhpSpec\ObjectBehavior;
+
+class LinkSpec extends ObjectBehavior {
+
+	function it_is_initializable()
+	{
+		$this->shouldHaveType('Bound1ess\MermaidPhp\Link');
+	}		
+
+}
---
+++ src/Bound1ess/MermaidPhp/Link.php
@@ -0,0 +1,7 @@
+<?php namespace Bound1ess\MermaidPhp;
+
+class Link {
+
+	# lovely Link class		
+
+}
