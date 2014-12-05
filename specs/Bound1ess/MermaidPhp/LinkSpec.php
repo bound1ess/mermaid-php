e92886738b8f4fc4c90eecea9b10da61f4e92bb9
Ilya <bound1ess.dev@gmail.com>
2014-12-05 20:54:44 +0600

    Add getNodes() and the constructor method for Link.

--- specs/Bound1ess/MermaidPhp/LinkSpec.php
+++ specs/Bound1ess/MermaidPhp/LinkSpec.php
@@ -1,12 +1,23 @@
 <?php namespace specs\Bound1ess\MermaidPhp;
 
 use PhpSpec\ObjectBehavior;
+use Bound1ess\MermaidPhp\Node;
 
 class LinkSpec extends ObjectBehavior {
+
+	function let(Node $node, Node $anotherNode)
+	{
+		$this->beConstructedWith($node, $anotherNode);
+	}
 
 	function it_is_initializable()
 	{
 		$this->shouldHaveType('Bound1ess\MermaidPhp\Link');
 	}		
+
+	function it_returns_both_nodes(Node $node, Node $anotherNode)
+	{
+		$this->getNodes()->shouldReturn([$node, $anotherNode]);
+	}
 
 }
--- src/Bound1ess/MermaidPhp/Link.php
+++ src/Bound1ess/MermaidPhp/Link.php
@@ -2,6 +2,27 @@
 
 class Link {
 
-	# lovely Link class		
+	/**
+	 * @var array
+	 */
+	protected $nodes = [];
+	
+	/**
+	 * @param Node $node
+	 * @param Node $anotherNode
+	 */		
+	public function __construct($node, $anotherNode)
+	{
+		$this->nodes[] = $node;
+		$this->nodes[] = $anotherNode;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getNodes()
+	{
+		return $this->nodes;
+	}
 
 }
