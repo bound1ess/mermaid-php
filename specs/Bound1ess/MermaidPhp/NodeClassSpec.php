027e13e14c53fea852407570cc9fe0d5d353d20b
bound1ess <bound1ess.dev@gmail.com>
2014-12-29 20:27:30 +0600

    Implement NodeClass.

--- specs/Bound1ess/MermaidPhp/NodeClassSpec.php
+++ specs/Bound1ess/MermaidPhp/NodeClassSpec.php
@@ -4,9 +4,30 @@ use PhpSpec\ObjectBehavior;
 
 class NodeClassSpec extends ObjectBehavior {
 
+    function let()
+    {
+        $this->beConstructedWith('className');
+    }
+
     function it_is_initializable()
 	{
         $this->shouldHaveType('Bound1ess\MermaidPhp\NodeClass');
     }			
+
+    function it_returns_the_class_name()
+    {
+        $this->getName()->shouldReturn('className');
+    }
+
+    function it_returns_class_properties()
+    {
+        $this->add('fill', '#ccf');
+        $this->callOnWrappedObject('__call', ['strokeWidth', ['20px']]);
+
+        $this->getProperties()->shouldReturn([
+            'fill'         => '#ccf',
+            'stroke-width' => '20px',
+        ]);
+    }
 
 }
--- src/Bound1ess/MermaidPhp/NodeClass.php
+++ src/Bound1ess/MermaidPhp/NodeClass.php
@@ -2,6 +2,71 @@
 
 class NodeClass {
 
-    // TODO
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var array
+     */
+    protected $properties = [];
+
+    /**
+     * @param string $name
+     * @return NodeClass
+     */
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @param string $property
+     * @param string $value
+     * @return void
+     */
+    public function add($property, $value)
+    {
+        $this->properties[$property] = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return array
+     */
+    public function getProperties()
+    {
+        return $this->properties;
+    }
+
+    /**
+     * @param string $property
+     * @param array $value
+     * @return void
+     */
+    public function __call($property, array $value)
+    {
+        $this->add($this->transform($property), $value[0]);    
+    }
+
+    /**
+     * @param string $value
+     * @return string
+     */
+    protected function transform($value)
+    {
+        return preg_replace_callback('/[A-Z]/', function(array $matches)
+        {
+            return '-'.strtolower($matches[0]);
+        }, $value);
+    }
 
 }
